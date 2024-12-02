3d1a2564c62d7085e2a082e09a3cc93810460988
Md Tabraiz <138916881+tabraiz06@users.noreply.github.com>
2024-12-02 19:24:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const inputField = document.getElementById("fname");
+
+// Attach an onblur event listener to the input field
+inputField.addEventListener("blur", function () {
+  // Convert the value of the input field to uppercase
+  inputField.value = inputField.value.toUpperCase();
+});
